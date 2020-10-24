5e6629ed04f2a9966c4c8d526a91d7ff1cd1d2e9
Christophe Thibaut <cthibaut@octo.com>
2020-10-24 13:21:32 +0200

    we have a failing test

--- LedDriver/test/LedDriverTest.c
+++ LedDriver/test/LedDriverTest.c
@@ -1,4 +1,5 @@
 #include "unity_fixture.h"
+#include "LedDriver.h"
 TEST_GROUP(LedDriver);
 
 TEST_SETUP(LedDriver)
@@ -11,7 +12,9 @@ TEST_TEAR_DOWN(LedDriver)
 
 TEST(LedDriver, LedsOffAfterCreate)
 {
-    TEST_FAIL_MESSAGE("Start here");
+    uint16_t virtualLeds = 0xffff;
+    LedDriver_Create(&virtualLeds);
+    TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
 }
 
 
