b45ceb90515a02b5a837836440447cb34b47eac3
Christophe Thibaut <cthibaut@octo.com>
2020-10-24 16:09:50 +0200

    practically done

--- LedDriver/src/LedDriver.c
+++ LedDriver/src/LedDriver.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include "LedDriver.h"
 #include "RuntimeError.h"
 
@@ -5,10 +6,26 @@ static uint16_t *ledsAddress;
 static uint16_t ledsImage;
 
 enum {ALL_LEDS_ON = ~0, ALL_LEDS_OFF = ~ALL_LEDS_ON};
+enum {FIRST_LED = 1, LAST_LED = 16};
 
 static uint16_t convertLedNumberToBit(int ledNumber)
 {
     return 1 << (ledNumber - 1);
+}
+
+static bool IsLedOutOfBounds(int ledNumber)
+{
+    return (ledNumber < FIRST_LED) || (ledNumber > LAST_LED);
+}
+
+static void setLedImageBit(int ledNumber)
+{
+    ledsImage |= convertLedNumberToBit(ledNumber);
+}
+
+static void clearLedImageBit(int ledNumber)
+{
+    ledsImage &= ~convertLedNumberToBit(ledNumber);
 }
 
 static void updateHardware(void)
@@ -25,12 +42,12 @@ void LedDriver_Create(uint16_t * address)
 
 void LedDriver_TurnOn(int ledNumber)
 {
-    if (ledNumber <= 0 || ledNumber > 16)
+    if (IsLedOutOfBounds(ledNumber))
     {
         RUNTIME_ERROR("LED Driver: out-of-bounds LED", ledNumber);
         return;
     }
-    ledsImage |= convertLedNumberToBit(ledNumber);
+    setLedImageBit(ledNumber);
     updateHardware();
 }
 
@@ -39,12 +56,17 @@ void LedDriver_TurnAllOn(void)
     ledsImage = ALL_LEDS_ON;
     updateHardware();
 }
+void LedDriver_TurnAllOff(void)
+{
+    ledsImage = ALL_LEDS_OFF;
+    updateHardware();
+}
 void LedDriver_TurnOff(int ledNumber)
 {
-    if (ledNumber <= 0 || ledNumber > 16)
+    if (IsLedOutOfBounds(ledNumber))
         return;
 
-    ledsImage &= ~(convertLedNumberToBit(ledNumber));
+    clearLedImageBit(ledNumber);
     updateHardware();
 }
 
@@ -52,3 +74,14 @@ void LedDriver_Destroy(void)
 {
 }
 
+bool LedDriver_IsOn(int ledNumber)
+{
+    if(IsLedOutOfBounds(ledNumber))
+        return false;
+    return ledsImage & (convertLedNumberToBit(ledNumber));
+}
+
+bool LedDriver_IsOff(int ledNumber)
+{
+    return !LedDriver_IsOn(ledNumber);
+}
--- LedDriver/src/LedDriver.h
+++ LedDriver/src/LedDriver.h
@@ -1,7 +1,11 @@
+#include <stdbool.h>
 #include <stdint.h>
 
 void LedDriver_Create(uint16_t * address);
 void LedDriver_TurnOn(int ledNumber);
 void LedDriver_TurnAllOn(void);
+void LedDriver_TurnAllOff(void);
 void LedDriver_TurnOff(int ledNumber);
 void LedDriver_Destroy(void);
+bool LedDriver_IsOn(int ledNumber);
+bool LedDriver_IsOff(int ledNumber);
